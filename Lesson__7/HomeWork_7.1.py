e5199a5b61e61bf53b95fc97f8c6f6da555b6b4a
gnatali081276-crypto <gnatali081276@gmail.com>
2026-07-13 12:16:05 +0300

    add homework 7.1

--- Lesson__7/HomeWork_7.1.py
+++ Lesson__7/HomeWork_7.1.py
@@ -17,3 +17,6 @@ print('ОК')
 # assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
 # assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
 # print('ОК')
+
+# функція працює правильно, після запуску файлу внизу консолі ми бачимо слово- "ОК"
+# ми повернули з функції правильний сформований рядок.
